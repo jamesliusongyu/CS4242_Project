dc5164120d0a189985557edcabca343c539258c4
jamesliusongyu <34643054+jamesliusongyu@users.noreply.github.com>
2025-02-14 15:59:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CS4242_Project
---
+++ code/model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+# extend MF to include content discovery and user features
+
+
+
+class MF(nn.Module):
+    def __init__(self, num_users, num_items, embedding_size=8, dropout=0, mean=0):
+        super(MF, self).__init__()
+        self.user_emb = nn.Embedding(num_users, embedding_size)
+        self.user_bias = nn.Embedding(num_users, 1)
+        self.item_emb = nn.Embedding(num_items, embedding_size)
+        self.item_bias = nn.Embedding(num_items, 1)
+
+        self.user_emb.weight.data.uniform_(0, 0.005)
+        self.user_bias.weight.data.uniform_(-0.01, 0.01)
+        self.item_emb.weight.data.uniform_(0, 0.005)
+        self.item_bias.weight.data.uniform_(-0.01, 0.01)
+
+        self.mean = nn.Parameter(torch.FloatTensor([mean]), False)
+        self.dropout = nn.Dropout(dropout)
+
+        self.num_user = num_users
+
+    def forward(self, u_id, i_id):
+        U = self.user_emb(u_id)
+        b_u = self.user_bias(u_id).squeeze()
+        I = self.item_emb(i_id)
+        b_i = self.item_bias(i_id).squeeze()
+        return self.dropout((U * I).sum(1) + b_u + b_i + self.mean)
